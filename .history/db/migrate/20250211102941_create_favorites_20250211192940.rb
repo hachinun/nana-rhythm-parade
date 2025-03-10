d2f3e434d0759e11b0e12e367d0fa9f256a005e9
hachi <myaoru@icloud.com>
2025-03-10 12:45:33 +0900

    dbの修正3

--- .history/db/migrate/20250211102941_create_favorites_20250211192940.rb
+++ .history/db/migrate/20250211102941_create_favorites_20250211192940.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[7.1]
   def change
     create_table :favorites do |t|
+      t.references :user, null: false, foreign_key: true
       t.references :video, null: false, foreign_key: true
-      t.string :session_id # もしsession_idが必要なら追加する
 
       t.timestamps
     end
