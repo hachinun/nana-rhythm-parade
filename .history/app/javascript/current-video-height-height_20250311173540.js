0c9e7fcc3832bc112af8856d71f437ca8a2e3827
hachi <myaoru@icloud.com>
2025-03-11 17:38:08 +0900

    スマホ用の表示ver8

---
+++ .history/app/javascript/current-video-height-height_20250311173540.js
@@ -0,0 +1,13 @@
+function updateVideoListHeight() {
+    const currentVideo = document.getElementById('current-video');
+    const videoList = document.getElementById('video-list');
+
+    if (currentVideo) {
+        const currentVideoHeight = currentVideo.clientHeight;
+        videoList.style.setProperty('--current-video-height', `${currentVideoHeight}px`);
+    }
+}
+
+// 動画のサイズが変更されたときに高さを更新
+window.addEventListener('resize', updateVideoListHeight);
+window.addEventListener('load', updateVideoListHeight);
